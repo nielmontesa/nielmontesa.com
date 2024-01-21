35ed7eee2c2429dea74dc60f25868daf39a42238
nielmontesa <nirumontesa@gmail.com>
2024-01-21 16:56:10 +0800

    Added a sitemap!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon({
-    include: {
-      mdi: ["*"] // (Default) Loads entire Material Design Icon set
-    }
-  }), mdx()]
+  site: "https://nielmontesa.com",
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        mdi: ["*"], // (Default) Loads entire Material Design Icon set
+      },
+    }),
+    mdx(),
+    sitemap(),
+  ],
 });
